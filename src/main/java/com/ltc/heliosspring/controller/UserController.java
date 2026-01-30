7b638ef22ef4e19079023d73253afd6fcae922a2
Ali <aaliyev14291@ada.edu.az>
2026-01-31 02:00:51 +0400

    User Controller

--- src/main/java/com/ltc/heliosspring/controller/UserController.java
+++ src/main/java/com/ltc/heliosspring/controller/UserController.java
@@ -17,5 +17,33 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public AppUser createUser(@RequestBody AppUser userEntity){
+        return userService.create(userEntity);
+    }
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<AppUser> getAll (){
+        return userService.getAll();
+    }
+
+    @GetMapping("/getById/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public AppUser getById(@PathVariable Long id){
+        return userService.getById(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public AppUser update(@PathVariable Long id, AppUser UserEntity){
+        return userService.update(id, UserEntity);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        userService.deleteById(id);
+    }
 }
